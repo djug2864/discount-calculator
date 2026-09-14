d01a8b327f5e4a897117d697bce7f5a9f5918d45
djug <119922512+djug2864@users.noreply.github.com>
2026-09-14 20:11:36 +0900

    Update AppInfoActivity.kt

--- app/src/main/java/com/example/discount/AppInfoActivity.kt
+++ app/src/main/java/com/example/discount/AppInfoActivity.kt
@@ -11,3 +11,19 @@ class AppInfoActivity : AppCompatActivity() {
         setContentView(R.layout.activity_app_info)
     }
 }
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class AppInfoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app_info)
+
+        findViewById<Button>(R.id.backButton).setOnClickListener {
+            finish()
+        }
+    }
+}
